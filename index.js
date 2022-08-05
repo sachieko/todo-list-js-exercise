5f2dee2e01a8f05c47e5412dc5f2616782ad8c0b
sachieko <kirazetsubou@gmail.com>
2022-08-05 12:26:51 -0600

    Refactored with methods

--- index.js
+++ index.js
@@ -5,19 +5,15 @@ const newTask = function(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+    completeTask: function() {
+      task.complete = true;
+    },
+    logTaskState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    }
   };
   return task;
-};
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-const completeTask = function(task) {
-  task.complete = true;
-};
-
-// Print the state of a task to the console in a nice readable way
-const logTaskState = function(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
 };
 
 // DRIVER CODE BELOW
@@ -26,7 +22,7 @@ const task1 = newTask("Clean Cat Litter", "There is a bunch of 💩💩 to remov
 const task2 = newTask("Do Laundry", "So much clothes"); // task 1
 const tasks = [task1, task2];
 
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
+task1.logTaskState(); // Clean Cat Litter has not been completed
+task1.completeTask();
+task1.logTaskState(); // Clean Cat Litter has been completed
 console.log(tasks);
